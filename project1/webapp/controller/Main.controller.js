981878ce5893060d58435e6f7fc0660c2c5f6030
BeomJin01 <jay011221@gmail.com>
2026-05-19 10:15:41 +0900

    git Test 1

---
+++ project1/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("code.t4.ui5.project1.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ project1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["code/t4/ui5/project1/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
