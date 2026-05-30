1786c4e79b1e3b415007d0e443b88d1e8e52eb43
BeomJin01 <jay011221@gmail.com>
2026-05-30 22:28:09 +0900

    mm04 git ver.2

--- project1/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-QUnit.config.autostart = false;
-
-sap.ui.require(["code/t4/ui5/project1/test/integration/AllJourneys"
-], function () {
-	QUnit.start();
-});
--- project1/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"code/t4/ui5/project1/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
